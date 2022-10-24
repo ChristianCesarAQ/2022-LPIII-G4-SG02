f238a88aa2cddffd988617981258bb95c7972bdd
christian cesar aguirre <christian.aguirre@ucsm.edu.pe>
2022-10-24 00:49:13 -0500

    GUI: Actividades y Ejercicios

---
+++ GUI/Actividades/Actividad 01/JCheckBox/PCasillaVerificacion.java
@@ -0,0 +1,13 @@
+package JButton;
+
+// Prueba de MarcoCasillaVerificacion.
+import javax.swing.JFrame;
+
+public class PCasillaVerificacion {
+    public static void main(String[] args) {
+        MarcoCasillaVerificacion marcoCasillaVerificacion = new MarcoCasillaVerificacion();
+        marcoCasillaVerificacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoCasillaVerificacion.setSize(275, 100);
+        marcoCasillaVerificacion.setVisible(true);
+    }
+} // fin de la clase PruebaCasillaVerificacion
