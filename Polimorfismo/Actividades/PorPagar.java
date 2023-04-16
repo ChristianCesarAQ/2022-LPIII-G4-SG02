032e5b88c3715041ade4c33d8856c7b02dd8e2c6
JoseM333 <102648655+JoseM333@users.noreply.github.com>
2023-04-16 11:46:22 -0500

    Delete Polimorfismo directory

--- Polimorfismo/Actividades/PorPagar.java
+++
@@ -1,8 +0,0 @@
-//nombre: PorPagar.java
-//descripcion: declaración de la interfaz PorPagar
-
-package Actividades;
-
-public interface PorPagar {
-	double obtenerMontoPago();//calcula el pago
-}//fin de la intefaz PorPagar
--- Polimorfismo/Ejercicios/Interfaz impacto ecologico/impactoEcologico.java
+++
@@ -1,7 +0,0 @@
-package ejercicio2;
-
-public interface impactoEcologico {
-	
-	double obtenerImpactoEcologico();
-
-}//fin interfaz impatoEcologico
